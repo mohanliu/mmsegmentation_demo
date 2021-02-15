f2db2841bfd6e97481950b449b3fc76d6244efc8
mohanliu <mohan@u.northwestern.edu>
2021-02-15 17:30:46 -0600

    update experiement configs

---
+++ experiments/config_standfordbackground.py
@@ -0,0 +1,151 @@
+# dataset settings
+dataset_type = "StandfordBackgroundDataset"
+data_root = "/rd-temp/mohan/iccv09Data"
+img_dir = "images"
+ann_dir = "labels"
+split_dir = "splits"
+
+num_classes = 8
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], 
+    to_rgb=True
+)
+image_scale_size = (320, 240)
+crop_size = (256, 256)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=image_scale_size, ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=image_scale_size,
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    samples_per_gpu=num_classes,
+    workers_per_gpu=num_classes,
+    train=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir=img_dir,
+        ann_dir=ann_dir,
+        split='{}/train.txt'.format(split_dir),
+        pipeline=train_pipeline
+    ),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir=img_dir,
+        ann_dir=ann_dir,
+        split='{}/val.txt'.format(split_dir),
+        pipeline=test_pipeline
+    ),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir=img_dir,
+        ann_dir=ann_dir,
+        split='{}/val.txt'.format(split_dir),
+        pipeline=test_pipeline
+    )
+)
+
+# model settings
+# Since we use ony one GPU, BN is used instead of SyncBN
+norm_cfg = dict(type='BN', requires_grad=True)
+
+model = dict(
+    type='EncoderDecoder',
+#     pretrained='open-mmlab://resnet50_v1c',
+    backbone=dict(
+        type='ResNetV1c',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        dilations=(1, 1, 2, 4),
+        strides=(1, 2, 1, 1),
+        norm_cfg=norm_cfg,
+        norm_eval=False,
+        style='pytorch',
+        contract_dilation=True
+    ),
+    decode_head=dict(
+        type='PSPHead',
+        in_channels=2048,
+        in_index=3,
+        channels=512,
+        pool_scales=(1, 2, 3, 6),
+        dropout_ratio=0.1,
+        num_classes=num_classes,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
+    ),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=1024,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=num_classes,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)
+    ),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole')
+)
+
+# yapf:disable
+log_config = dict(
+    interval=25,
+    hooks=[
+        dict(type='TextLoggerHook', by_epoch=False),
+        # dict(type='TensorboardLoggerHook')
+    ]
+)
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+cudnn_benchmark = True
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer_config = dict()
+
+# learning policy
+lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
+
+# runtime settings
+runner = dict(type='IterBasedRunner', max_iters=100)
+checkpoint_config = dict(by_epoch=False, interval=25)
+evaluation = dict(interval=50, metric='mIoU')
